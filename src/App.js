6642796cbba07ae16d94809a487a1b5aa893abe9
sai preetham ravula <160527233+VSPR1@users.noreply.github.com>
2024-06-17 23:10:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import  "bulma/css/bulma.css";
+
+
+import SignupForm from "./components/SignupForm.js";
+import CoolButton from "./components/CoolButton.js";
 
 function App() {
   return (
@@ -18,6 +23,13 @@ function App() {
           Learn React
         </a>
       </header>
+             <SignupForm />
+
+      <CoolButton isSuccess>Button 1</CoolButton>
+      <CoolButton> Button 2 </CoolButton>
+      <div><CoolButton  isSmall  isDanger  isRounded>Button 1</CoolButton>
+      <CoolButton  isSmall  isSuccess>Button 2</CoolButton></div>
+
     </div>
   );
 }
